edd9af1899dfb26695b440e8f8c0b7829d144368
Yevhenii Kozarenko <kozarenk007@gmail.com>
2020-09-13 14:41:40 +0300

    Updates

---
+++ precache-manifest.a547efa55cad2b5939e8f429a55c2574.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d27beadcbcd36986a2d27a63f36fd0b5",
+    "url": "/cursorHW_15/index.html"
+  },
+  {
+    "revision": "3e1df0711be5d2e24e9e",
+    "url": "/cursorHW_15/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e8b72c515046d4f91c7e",
+    "url": "/cursorHW_15/static/js/2.113a44fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cursorHW_15/static/js/2.113a44fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e1df0711be5d2e24e9e",
+    "url": "/cursorHW_15/static/js/main.408399cc.chunk.js"
+  },
+  {
+    "revision": "7a89c9c8cee1af51b5d0",
+    "url": "/cursorHW_15/static/js/runtime-main.40ad8bd3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cursorHW_15/static/media/logo.5d5d9eef.svg"
+  }
+]);
